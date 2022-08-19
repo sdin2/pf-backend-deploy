18473cb9de39bae4643bd9fdc1075a402004389d
sdin2 <desat_94@hotmail.com>
2022-08-19 13:21:43 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -7,6 +7,13 @@ const { DATABASE_URL } = process.env;
 const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 const basename = path.basename(__filename);
 
