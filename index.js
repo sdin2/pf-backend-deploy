3709f007916e976667e899ad83393e2f2154d9fb
sdin2 <desat_94@hotmail.com>
2022-08-19 12:55:29 -0300

    CA

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { PORT } = process.env;
 
+let port = PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
