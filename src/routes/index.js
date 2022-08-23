276cd8897d1aa47ea3d47cf4b236c8e1edc36307
sdin2 <desat_94@hotmail.com>
2022-08-22 21:13:23 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ const { Router } = require("express");
 const router = Router();
 const newsRoute = require("./newsRoute");
 const userRoute = require("./userRoute");
+const genreRoute = require("./genreRoute");
+const gamesRoute = require("./gamesRoute");
 
 router.use("/news", newsRoute);
 router.use("/users", userRoute);
