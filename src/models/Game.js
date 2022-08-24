9c3e37a20e5faab50062a697dec3bec379f37ca7
sdin2 <desat_94@hotmail.com>
2022-08-24 01:22:27 -0300

    Update Game.js

--- src/models/Game.js
+++ src/models/Game.js
@@ -3,13 +3,14 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define("game", {
     id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
       allowNull: false,
+      primaryKey: true,
     },
     name: {
       type: DataTypes.STRING,
-      // allowNull: false,
+      allowNull: false,
     },
     img: {
       type: DataTypes.STRING,
