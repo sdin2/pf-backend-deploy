16c926008595268ce4fc2de201821c56bd52ed55
sdin2 <desat_94@hotmail.com>
2022-08-24 01:39:58 -0300

    ASDF

--- src/models/Game.js
+++ src/models/Game.js
@@ -3,14 +3,14 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define("game", {
     apiId: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false,
+      primaryKey: true,
     },
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       allowNull: false,
-      primaryKey: true,
     },
     name: {
       type: DataTypes.STRING,
