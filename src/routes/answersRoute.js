583d25c03ed14fa0389e2b81a06aacf27aa67e4f
sdin2 <desat_94@hotmail.com>
2022-08-26 12:30:34 -0300

    Update answersRoute.js

--- src/routes/answersRoute.js
+++ src/routes/answersRoute.js
@@ -1,12 +1,12 @@
 const { axios } = require("axios");
 const express = require("express");
 const router = express.Router();
-const { Anwser, User } = require("../db.js");
+const { Answer, User } = require("../db.js");
 
 router.post("/", async (req, res, next) => {
   const forum = req.body;
   try {
-    const createComment = await Anwser.create({
+    const createComment = await Answer.create({
       comment: forum.comment,
     });
     res.send("Comment posted!");
@@ -17,7 +17,7 @@ router.post("/", async (req, res, next) => {
 
 router.get("/", async (req, res, next) => {
   try {
-    const forumData = await Anwser.findAll();
+    const forumData = await Answer.findAll();
     res.send(forumData);
   } catch (error) {
     next(error);
@@ -28,7 +28,7 @@ router.put("/:id", async (req, res, next) => {
   const id = req.params.id;
   const allBody = req.body;
   try {
-    let forumData = await Anwser.findByPk(id);
+    let forumData = await Answer.findByPk(id);
     await forumData.update({
       comment: allBody.comment,
       like: allBody.like,
