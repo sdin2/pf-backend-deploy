d4754f530108d6f2a8bd441f41f90f21c2679150
sdin2 <desat_94@hotmail.com>
2022-08-26 02:09:18 -0300

    Update gamesRoute.js

--- src/routes/gamesRoute.js
+++ src/routes/gamesRoute.js
@@ -10,12 +10,10 @@ const router = Router();
 router.get("/", async (req, res, next) => {
   try {
     const name = req.query.name ? req.query.name : req.body.name;
-    console.log("name", name);
     if (name) {
       let videogameByName = await axios.get(
         `https://api.rawg.io/api/games?key=${API_KEY_GAMES}&search=${name}`
       );
-      console.log("videogames", videogameByName);
       videogameByName = videogameByName.data.results.map((game) => {
         return {
           id: game.id,
