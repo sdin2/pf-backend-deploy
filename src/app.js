f4dcfcdb7d3bc5f466951b2524b5be6b2689e8ab
sdin2 <desat_94@hotmail.com>
2022-08-26 12:01:54 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ server.post("/api/checkout", async (req, res) => {
 server.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
   const status = err.status || 500;
-  const message = err.message || err;
+  const message = err;
   console.error(err);
   res.status(status).send(message);
 });
