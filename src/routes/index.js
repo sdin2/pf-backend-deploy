a22dc1d3dc7116d8da5670bcca999dca95d93a3b
sdin2 <desat_94@hotmail.com>
2022-08-26 11:28:15 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Router } = require("express");
+const { auth } = require("express-openid-connect");
 const router = Router();
 
 const newsRoute = require("./newsRoute");
@@ -8,6 +9,9 @@ const genreRoute = require("./genreRoute");
 const gamesRoute = require("./gamesRoute");
 const forumRoute = require("./forumRoute");
 const rewardRoute = require("./rewardRoute");
+const missionsRoute = require("./missionsRoute");
+const forumCommentsRoutes = require("./forumCommentsRoute");
+const paymentRoute = require("./paymentRoute");
 
 router.use("/news", newsRoute);
 router.use("/users", userRoute);
@@ -15,5 +19,8 @@ router.use("/genre", genreRoute);
 router.use("/games", gamesRoute);
 router.use("/forum", forumRoute);
 router.use("/reward", rewardRoute);
+router.use("/missions", missionsRoute);
+router.use("/anwsers", forumCommentsRoutes);
+router.use("/payment", paymentRoute);
 
 module.exports = router;
