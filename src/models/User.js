cb54a30201834782b6b4a79dfa1a97137c12c130
sdin2 <desat_94@hotmail.com>
2022-08-27 16:00:44 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -19,7 +19,7 @@ module.exports = (sequelize) => {
       unique: true,
     },
     img: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allownull: true,
       defaultValue: "https://j.gifs.com/ygdY27.gif",
     },
