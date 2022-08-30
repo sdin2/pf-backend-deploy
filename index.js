7413cade41e3b69c10b12e1e768b8e95c50995b6
sdin2 <desat_94@hotmail.com>
2022-08-30 16:37:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require("./src/app.js");
+const socketServerIo = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { PORT } = process.env;
 const { saveAllGamesInDb } = require("./src/controllers/GamesController");
@@ -6,7 +6,7 @@ const { saveAllGamesInDb } = require("./src/controllers/GamesController");
 let port = PORT || 3001;
 // Syncing all the models at once.
 conn.sync().then(() => {
-  server.listen(PORT, () => {
+  socketServerIo.listen(PORT, () => {
     saveAllGamesInDb();
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
