09ac2bda5fdb87f61c7f125c73ffeb24ea8beb2d
sdin2 <desat_94@hotmail.com>
2022-08-31 10:40:11 -0300

    Update userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -127,7 +127,7 @@ router.put("/:id", async (req, res, next) => {
 });
 
 router.get("/available/:nickname", async (res, req) => {
-  const { nickname } = req.params;
+  const nickname = req.params;
   console.log(nickname);
   const nicknameAvailable = await User.finOne({
     where: { nickname: nickname },
