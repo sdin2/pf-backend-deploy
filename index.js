69d11636a3a64c06b0350d21a26fe6f96a8d8599
sdin2 <desat_94@hotmail.com>
2022-08-31 12:01:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ let PORT = process.env.PORT;
 // Syncing all the models at once.
 conn.sync().then(() => {
   socketServerIo.listen(PORT, () => {
-    saveAllGenresInDb();
-    // saveAllGamesInDb();
+    // saveAllGenresInDb();
+    saveAllGamesInDb();
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
